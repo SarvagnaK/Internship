05d26d2321ac092354c67acb5970a4acd20a8ac9
SarvagnaK <khatri.sarvagna@gmail.com>
2025-02-24 16:22:52 +0530

    directives

--- Angular/src/app/admin/admin.component.ts
+++ Angular/src/app/admin/admin.component.ts
@@ -1,12 +1,27 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
-  imports: [RouterLink],
+  imports: [RouterLink,NgClass,FormsModule,NgStyle],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
 
+divBgColor:string='bg-success';
+div1BgColor:string='';
+
+isChecked: boolean=false;
+
+addDiv1Color(className:string )
+{
+  this.divBgColor=className;
+}
+addDiv3Color(color:string)
+{
+  this.div1BgColor=color;
+}
 }
