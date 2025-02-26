1f4aa69ef6e01272b4fc73e0f7f3bd1453a61e02
SarvagnaK <khatri.sarvagna@gmail.com>
2025-02-26 10:16:23 +0530

    post delete api

--- Angular/src/app/user/user.component.ts
+++ Angular/src/app/user/user.component.ts
@@ -1,16 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
 
-  http=inject(HttpClient);
+  http=inject(HttpClient); // Dependency Injection
   userList: any[] = []; 
+  userObj: any= {
+    "userId": 0,
+    "userName": "",
+    "emailId": "",
+    "fullName": "",
+    "role": "",
+    "createdDate": new Date(),
+    "password": "",
+    "projectName": "",
+    "refreshToken": "",
+    "refreshTokenExpiryTime": new Date()
+  }
+
   constructor(){
     this.getUsers();
   }
@@ -21,14 +35,28 @@ export class UserComponent {
     })
   }
 
-  closeSidePanel()
-  {
-    console.log("hi");
-    
+  onSaveUser(){
+    debugger;
+    this.http.post("/api/Complaint/AddNewUser",this.userObj).subscribe((res:any)=>{
+      debugger;
+      this.getUsers();
+    })
   }
-  openSidePanel()
+  onEdit(data:any)
   {
-    console.log("opened ");
-    
+    this.userObj=data;
+  }
+  onUpdate()
+  {
+    this.http.post("/api/Complaint/UpdateUser",this.userObj).subscribe((res:any)=>{
+      // debugger;
+      this.getUsers();
+    })
+  }
+  onDeleteUser(id:number){
+    this.http.delete("/api/Complaint/DeleteUserByUserId?userId="+id).subscribe((res:any)=>{
+      debugger;
+      this.getUsers();
+    })
   }
 }
